7ec99ccc546f5014e8253df6a8b8bfc3677a3df6
Maksym203 <99950289+Maksym203@users.noreply.github.com>
2023-09-19 09:43:17 +0200

    ImGui apparently functional

--- Primal-Engine/ModuleRenderer3D.cpp
+++ Primal-Engine/ModuleRenderer3D.cpp
@@ -4,6 +4,10 @@
 #include "SDL\include\SDL_opengl.h"
 #include <gl/GL.h>
 #include <gl/GLU.h>
+#include <stdio.h>
+#include "ImGui/imgui.h"
+#include "ImGui/backends/imgui_impl_sdl.h"
+#include "ImGui/backends/imgui_impl_opengl3.h"
 
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 #pragma comment (lib, "glu32.lib") /* link Microsoft OpenGL lib   */
